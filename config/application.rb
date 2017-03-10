679552bd83baebaa3f989c434800650e353f3bed
suaad <we_oo@hotmauil.com>
2017-03-10 16:54:27 +0300

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
-
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,5 +12,6 @@ module Samm
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-   @user       = users(:michael)
-   @other_user = users(:archer)
- end
+     @user = users(:michael)
+     @other_user = users(:archer)
+   end
  test "should redirect index when not logged in" do
    get users_path
    assert_redirected_to login_url
@@ -41,4 +41,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+
+
+  test "should redirect following when not logged in" do
+      get following_user_path(@user)
+      assert_redirected_to login_url
+  end
+
+    test "should redirect followers when not logged in" do
+      get followers_user_path(@user)
+      assert_redirected_to login_url
+    end
 end
